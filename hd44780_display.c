8983ce95433a4edf675bc1769be5de83c910e85a
Vladimir Meshkov <ubob74@gmail.com>
2020-01-29 20:56:38 +0300

    Display base support

--- hd44780_display.c
+++ hd44780_display.c
@@ -7,9 +7,143 @@
 #include <linux/i2c.h>
 #include <linux/delay.h>
 
+enum hd44780_pins {
+	RS,
+	RW,
+	E,
+	BL,
+	D4,
+	D5,
+	D6,
+	D7,
+};
+
+struct hd44780 {
+	struct i2c_adapter *adap;
+	struct i2c_msg msg[1];
+	unsigned char data;
+};
+
+static struct hd44780 hd44780;
+
+static void hd44780_init(struct i2c_client *client)
+{
+	hd44780.adap = client->adapter;
+	hd44780.msg->addr = client->addr;
+	hd44780.msg->flags = 0;
+	hd44780.msg->len = 1;
+	hd44780.msg->buf = &hd44780.data;
+}
+
+static int hd44780_transfer(void)
+{
+	return i2c_transfer(hd44780.adap, hd44780.msg, 1);
+}
+
+static int hd44780_bl(int bl)
+{
+	if (bl)
+		hd44780.data |= 1 << BL;
+	else
+		hd44780.data &= ~(1 << BL);
+	udelay(20);
+	return hd44780_transfer();
+}
+
+static int hd44780_strobe(void)
+{
+	int ret;
+
+	hd44780.data |= 1 << E;
+	udelay(20);
+	ret = hd44780_transfer();
+
+	hd44780.data &= ~(1 << E);
+	udelay(40);
+	ret = hd44780_transfer();
+
+	return ret;
+}
+
+static int hd44780_write(unsigned char val, int rs, int timeout)
+{
+	int ret;
+	int i;
+
+	if (rs)
+		hd44780.data |= rs << RS;
+	else
+		hd44780.data &= ~(rs << RS);
+
+	hd44780.data &= ~(0xF << 4);
+	for (i = 4; i < 8; i++)
+		hd44780.data |= !!(val & BIT(i)) << i;
+
+	pr_debug("high nibble: data=%X\n", hd44780.data);
+
+	ret = hd44780_transfer();
+	pr_err("transfer ret=%d\n", ret);
+
+	hd44780_strobe();
+
+	hd44780.data &= ~(0xF << 4);
+	for (i = 0; i < 4; i++)
+		hd44780.data |= !!(val & BIT(i)) << (i + D4);
+
+	pr_debug("low nibble: data=%X\n", hd44780.data);
+
+	ret = hd44780_transfer();
+
+	hd44780_strobe();
+
+	if (timeout)
+		udelay(timeout);
+
+	return ret;
+}
+
+static int hd44780_cmd(unsigned char cmd)
+{
+	return hd44780_write(cmd, 0, 120);
+}
+
+static int hd44780_data(unsigned char data)
+{
+	return hd44780_write(data, 1, 45);
+}
+
+static void hd44780_blink_test(void)
+{
+	hd44780_bl(1);
+	mdelay(500);
+	hd44780_bl(0);
+	mdelay(500);
+	hd44780_bl(1);
+}
+
 static int hd44780_probe(struct i2c_client *client, const struct i2c_device_id *id)
 {
-	pr_err("%s: enter\n", __func__);
+	int i;
+	unsigned char cmd[] = {
+		0x02, /* return home */
+		0x20, /* 4-bit mode */
+		0x0C, /* display on, cursor off, blinking off */
+		0x06, /* cursor increment */
+		0x01, /* clear display */
+	};
+	unsigned char d[] = {0x54, 0x65, 0x73, 0x74, 0x21, 0x2A};
+
+	hd44780_init(client);
+
+	hd44780_blink_test();
+
+	for (i = 0; i < ARRAY_SIZE(cmd); i++)
+		hd44780_cmd(cmd[i]);
+
+	/* Data */
+	for (i = 0; i < ARRAY_SIZE(d); i++)
+		hd44780_data(d[i]);
+
 	return 0;
 }
 
